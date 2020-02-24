4e1c36adb66a948bbfa0af26681776b982b5d098
Corbin Fox <corbin.fox@gmail.com>
2020-02-24 11:32:03 -0700

    Wired in ravend rpc dependency.  Use these environment variables:
    RAVEN_RPC_USER
    RAVEN_RPC_PASSWORD
    RAVEN_RPC_PROTOCOL (default: http)
    RAVEN_RPC_HOST (default: 127.0.0.1)
    RAVEN_RPC_PORT (default: 18766)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var raven = require('../lib/raven');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var title = 'Raven Express';
+  raven.getBlockchainInfo(function(err, ret) {
+    if (err) {
+      res.render('raven-help', { title: title, info: 'raven is not responding' });
+    } else {
+      res.render('index', { title: title, info: JSON.stringify(ret.result, null, 4) });
+    }
+  });
 });
 
 module.exports = router;
