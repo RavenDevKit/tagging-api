ef036fe4bebf0bb3309ad01aee8856ac3b955f88
Corbin Fox <corbin.fox@gmail.com>
2020-02-27 11:21:32 -0700

    Added error message to raven-help page

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   var title = 'Raven Express';
   raven.getBlockchainInfo(function(err, ret) {
     if (err) {
-      res.render('raven-help', { title: title, info: 'raven is not responding' });
+      res.render('raven-help', { title: title, info: 'raven is not responding', error: err });
     } else {
       res.render('index', { title: title, info: JSON.stringify(ret.result, null, 4) });
     }
